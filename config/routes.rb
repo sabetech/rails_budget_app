6302730528213f141c13cc36004278d0eb9736f1
Albert Mensah-Ansah <albert.mensahansah@gmail.com>
2022-10-15 12:26:16 +0000

    Wire up UI of budget app

--- config/routes.rb
+++ config/routes.rb
@@ -12,12 +12,11 @@ Rails.application.routes.draw do
   end
   root 'home#index'
   resources :groups
+  resources :expenses
   
-  # resources :expenses
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
   
 end
